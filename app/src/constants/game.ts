89466ca4c061f326834b058ff844ae3d9e1a58cb
BuildTools <unconfigured@null.spigotmc.org>
2023-11-24 13:36:03 +0900

    update

--- app/src/constants/game.ts
+++ app/src/constants/game.ts
@@ -1,32 +1,55 @@
-// const GAMES = [
-//   {
-//     title: "ほうれん草ゲーム",
-//     description: {
-//       overview: "",
-//       url: ""
-//     }
-//   },
-//   {
-//     title: "ほうれん草ゲーム",
-//     description: {
-//       overview: "",
-//       url: ""
-//     }
-//   },
-//   {
-//     title: "ほうれん草ゲーム",
-//     description: {
-//       overview: "",
-//       url: ""
-//     }
-//   },
-//   {
-//     title: "ほうれん草ゲーム",
-//     description: {
-//       overview: "",
-//       url: ""
-//     }
-//   }
-// ];
+const GAMES = [
+  {
+    title: "ほうれん草ゲーム",
+    description: {
+      overview: "ゲームの説明",
+      url: "https://w.atwiki.jp/nomikai-game/pages/35.html"
+    }
+  },
+  {
+    title: "ルパン三世ゲーム",
+    description: {
+      overview: "ゲームの説明",
+      url: "ゲームのURL"
+    }
+  },
+  {
+    title: "シュビドゥバドゥゲーム",
+    description: {
+      overview: "ゲームの説明",
+      url: "https://drinkgame.net/game10/"
+    }
+  },
+  {
+    title: "炙りカルビゲーム",
+    description: {
+      overview: "ゲームの説明",
+      url: "https://w.atwiki.jp/nomikai-game/pages/21.html"
+    }
+  },
+  {
+    title: "サイコロゲーム",
+    description: {
+      overview: "ゲームの説明",
+      url: "ゲームのURL"
+    }
+  },
+  {
+    title: "ピンポンパンゲーム",
+    description: {
+      overview: "ゲームの説明",
+      url: "https://w.atwiki.jp/nomikai-game/pages/17.html"
+    }
+  },
+  {
+    title: "トランプゲーム",
+    description: {
+      overview: "ゲームの説明",
+      url: "ゲームのURL"
+    }
+  }
+]
 
-// const getRandomGames = () => {};
+export const getRandomGame = () => {
+  return GAMES[Math.floor(Math.random() * GAMES.length)]
+}
---
+++ app/src/constants/index.ts
@@ -0,0 +1,1 @@
+export { getRandomGame } from "@/constants/game"
